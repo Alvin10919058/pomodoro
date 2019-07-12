6d8f527f7faa844b4a1c6164f569e079e40408e1
Alvin <zero840108@gmail.com>
2019-07-12 10:00:08 +0800

    feat: add item section

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/tool-list">tool list</router-link>
-    </div>
+    </div> -->
     <router-view />
   </div>
 </template>
@@ -15,6 +15,11 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+html,
+body {
+  margin: 0px;
+  background-color: #003164;
 }
 #nav {
   padding: 30px;
---
+++ src/views/Index.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <div class="left-section">
+      <div class="container">
+        <div class="add-to-do-item">
+          <input type="text" placeholder="ADD A NEW MISSION..." />
+          <i class="add-item-icon fas fa-plus"></i>
+        </div>
+        <div class="now-to-do-item">
+          <input type="radio" />
+          <p>the first thing today</p>
+        </div>
+        <div class="time-counter">25:00</div>
+        <div class="to-do-list-section">
+          <ul>
+            <li>
+              <input type="radio" />
+              <p>the second thing today</p>
+            </li>
+            <li>
+              <input type="radio" />
+              <p>the third thing today</p>
+            </li>
+            <li>
+              <input type="radio" />
+              <p>the forth thing today</p>
+            </li>
+          </ul>
+          <span>more</span>
+        </div>
+      </div>
+    </div>
+    <!-- <div class="right-section">2312312</div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+.left-section {
+  box-sizing: border-box;
+  padding: 48px 100px;
+  background-color: #ffedf7;
+  //   display: inline-block;
+  width: 65%;
+  height: 100vh;
+  .container {
+    //margin: 0px 100px 0px 100px;
+    .add-to-do-item {
+      // background-color: #fff;
+      width: 32vw;
+      // height: 28px;
+      // margin: auto;
+      display: flex !important;
+      input {
+        font-size: 16px;
+        color: #ff4384 !important;
+        padding: 1rem !important;
+        width: 100% !important;
+        border: 0 !important;
+        outline: none;
+      }
+      input::placeholder {
+        font-size: 16px;
+        font-weight: 600;
+        color: #ff4384 !important;
+      }
+      .add-item-icon {
+          cursor: pointer;
+        color: #ff4384 !important;
+        padding: 15px 15px 0px 0px;
+        background-color: #fff;
+        font-size: 20px;
+      }
+    }
+  }
+}
+// .right-section {
+//   display: inline-block;
+//   width: 35%;
+//   height: 100vh;
+//   background-color: #003164;
+// }
+</style>
